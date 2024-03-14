f49347916fee5c2626819af4ddef6c8ade510c7d
Joshua Chen <joshuawchen@icloud.com>
2024-03-14 11:10:29 -0500

    use automated dictionary unpacking with *_keys required naming convention

--- applications/poisson/dino_training/cm_weighted_training.py
+++ applications/poisson/dino_training/cm_weighted_training.py
@@ -15,152 +15,178 @@
 # Author: Joshua Chen and Tom O'Leary-Roseberry
 # Contact: joshuawchen@icloud.com | tom.olearyroseberry@utexas.edu
 
+#Typical Useage: python train.py --CLI args... 
+#				nn = train_dino_in_embedding_space(config_dict)
+#				train_nn_regressor(data,...)
 import sys
 import numpy as np
 import jax.random as jr
-from argparse import ArgumentParser, BooleanOptionalAction
+
 from typing import Dict, Iterable
 # sys.path.append( os.environ.get('DINOJAX_PATH'))
 sys.path.append('../../../') #temporary
 
-from dinojax import train_dino_in_embedding_space
+from dinojax import train_dino_in_embedding_space #this will be in the train.py file already
 
 def subdict(*, parent_dict: Dict, keys: Iterable):
 	return {k: parent_dict[k] for k in keys}
+import inspect
+def autodict(*args):
+	get_rid_of = ['autodict(', ',', ')', '\n']
+	print(inspect.currentframe())
+	calling_code = inspect.getouterframes(inspect.currentframe())#[0] #[0]
+	print(calling_code)
+	calling_code = calling_code[calling_code.index('autodict'):]
+	for garbage in get_rid_of:
+		calling_code = calling_code.replace(garbage, '')
+	var_names, var_values = calling_code.split(), args
+	dyn_dict = {var_name.removesuffix('_keys'): var_value for var_name, var_value in
+				zip(var_names, var_values)}
+	return dyn_dict
 
-################################################################################
-# Define CLI arguments 
-################################################################################
-parser = ArgumentParser(add_help=True)
+def main() -> int:
+	""""""
+	################################################################################
+	# Define CLI arguments 
+	################################################################################
+	from argparse import ArgumentParser, BooleanOptionalAction
+	
+	parser = ArgumentParser(add_help=True)
 
-# Random seed parameter
-parser.add_argument('-run_seed', '--run_seed', type=int, default=7, help="Seed for NN initialization/ data shuffling / initialization")
+	# Random seed parameter
+	parser.add_argument('-run_seed', '--run_seed', type=int, default=7, help="Seed for NN initialization/ data shuffling / initialization")
 
-# Neural Network Architecture parameters
-parser.add_argument("-architecture", dest='architecture',required=False, default = 'generic_dense', help="architecture type: as_dense or generic_dense",type=str)
-# parser.add_argument("-decoder", dest='decoder',required=False, default = 'jjt',  help="output basis: pod or jjt",type=str)
-parser.add_argument("-fixed_input_rank", dest='fixed_input_rank',required=False, default = 200, help="rank for input of AS network",type=int)
-parser.add_argument("-fixed_output_rank", dest='fixed_output_rank',required=False, default = 50, help="rank for output of AS network",type=int)
-parser.add_argument("-truncation_dimension", dest='truncation_dimension',required=False, default = 200, help="truncation dimension for low rank networks",type=int)
+	# Neural Network Architecture parameters
+	parser.add_argument("-architecture", dest='architecture',required=False, default = 'generic_dense', help="architecture type: as_dense or generic_dense",type=str)
+	# parser.add_argument("-decoder", dest='decoder',required=False, default = 'jjt',  help="output basis: pod or jjt",type=str)
+	parser.add_argument("-fixed_input_rank", dest='fixed_input_rank',required=False, default = 200, help="rank for input of AS network",type=int)
+	parser.add_argument("-fixed_output_rank", dest='fixed_output_rank',required=False, default = 50, help="rank for output of AS network",type=int)
+	parser.add_argument("-truncation_dimension", dest='truncation_dimension',required=False, default = 200, help="truncation dimension for low rank networks",type=int)
 
-# Neural Network Serialization parameters
-parser.add_argument("-network_name", dest='network_name',required=True,  help="out name for the saved weights",type=str)
+	# Neural Network Serialization parameters
+	parser.add_argument("-network_name", dest='network_name',required=True,  help="out name for the saved weights",type=str)
 
-# Data (Directory Location/Training) parameters
-parser.add_argument("-data_dir", dest='data_dir',required=True,  help="Directory where training data lies",type=str)
-parser.add_argument("-train_data_size", dest='train_data_size',required=False, default = 500,  help="training data size",type=int)
-parser.add_argument("-test_data_size", dest='test_data_size',required=False, default = 500,  help="testing data size",type=int)
+	# Data (Directory Location/Training) parameters
+	parser.add_argument("-data_dir", dest='data_dir',required=True,  help="Directory where training data lies",type=str)
+	parser.add_argument("-train_data_size", dest='train_data_size',required=False, default = 500,  help="training data size",type=int)
+	parser.add_argument("-test_data_size", dest='test_data_size',required=False, default = 500,  help="testing data size",type=int)
 
-# Optimization parameters
-parser.add_argument("-optax_optimizer", dest='optax_optimizer',required=False, default = 'adam',  help="Name of the optax optimizer to use",type=str)
-parser.add_argument("-n_epochs", dest='n_epochs',required=False, default = 1000,  help="number of epochs for training",type=int)
-parser.add_argument('-batch_size', dest='batch_size', type = int, default = 20, help = 'gradient batch size')
-parser.add_argument('-step_size', '--step_size', type=float, default=1e-3, help="What step size or 'learning rate'?")
+	# Optimization parameters
+	parser.add_argument("-optax_optimizer", dest='optax_optimizer',required=False, default = 'adam',  help="Name of the optax optimizer to use",type=str)
+	parser.add_argument("-n_epochs", dest='n_epochs',required=False, default = 1000,  help="number of epochs for training",type=int)
+	parser.add_argument('-batch_size', dest='batch_size', type = int, default = 20, help = 'gradient batch size')
+	parser.add_argument('-step_size', '--step_size', type=float, default=1e-3, help="What step size or 'learning rate'?")
 
-# Loss function parameters
-parser.add_argument("-l2_weight", dest='l2_weight',required=False, default = 1.,  help="weight for l2 term",type=float)
+	# Loss function parameters
+	parser.add_argument("-l2_weight", dest='l2_weight',required=False, default = 1.,  help="weight for l2 term",type=float)
+	parser.add_argument("-h1_weight", dest='h1_weight',required=False, default = 1.,  help="weight for h1 term",type=float)
 
-parser.add_argument("-h1_weight", dest='h1_weight',required=False, default = 1.,  help="weight for h1 term",type=float)
+	# Encoder/Decoder parameters
+	parser.add_argument('-rb_dir', '--rb_dir', type=str, default='../reduced_bases/', help="Where are the reduced bases")
+	parser.add_argument('-encoder_basis', '--encoder_basis', required=False, type=str, default='as', help="What type of input basis? Choose from [kle, as] ")
+	parser.add_argument('-decoder_basis', '--decoder_basis', type=str, default='pod', help="What type of input basis? Choose from [pod] ")
+	parser.add_argument('-save_embedded_data', '--save_embedded_data', help="Should we save the embedded training data to disk or just use it in training without saving to disk. WIthout this flag, defaults to false", default=False, action=BooleanOptionalAction)
+	# parser.add_argument('-J_data', '--J_data', type=int, default=1, help="Is there J data??? ")
+	################################################################################
+	# Parse arguments and place them in a heirarchical config dictionary 	       #
+	################################################################################
+	cli_args = vars(parser.parse_args())
 
+	###################################################################################
+	# Define the keys for each configuration dict (*_keys is a required naming 
+	# convention here, since we define the configuration dict names by *
+	##################################################################################
+	nn_keys = ('architecture',) #'depth','layer_width', 'activation'
+	data_keys =  ('data_dir','train_data_size','test_data_size',) #'data_filenames'
+	training_keys =  ('step_size','batch_size','optax_optimizer','n_epochs')
+	network_serialization_keys =  ('network_name',)
+	config_dict = \
+		{k.removesuffix('_keys'): subdict(parent_dict = cli_args, keys = v) 
+		for k, v in locals().items() if k.endswith('_keys')
+		}
+	print(config_dict)
+	
+	# problem_config_dict = {} #is this necessary
+	# config_dict['forward_problem'] = problem_config_dict
+	
+	#TEMPORARY, modify additionally each configuraiton dict. THese really should be 
+	#CLI commands of some sort, or part of the default associated with CLI params
+	#e.g. nn['generic_dense'] default.
 
-# Encoder/Decoder parameters
-parser.add_argument('-rb_dir', '--rb_dir', type=str, default='../reduced_bases/', help="Where are the reduced bases")
-parser.add_argument('-encoder_basis', '--encoder_basis', required=False, type=str, default='as', help="What type of input basis? Choose from [kle, as] ")
-parser.add_argument('-decoder_basis', '--decoder_basis', type=str, default='pod', help="What type of input basis? Choose from [pod] ")
-parser.add_argument('-save_embedded_data', '--save_embedded_data', help="Should we save the embedded training data to disk or just use it in training without saving to disk. WIthout this flag, defaults to false", default=False, action=BooleanOptionalAction)
-# parser.add_argument('-J_data', '--J_data', type=int, default=1, help="Is there J data??? ")
-################################################################################
-# Parse arguments and place them in a heirarchical config dictionary 	       #
-################################################################################
-cli_args = vars(parser.parse_args())
+	# Neural Network Architecture parameters
+	config_dict['nn']['depth'] = 6 
+	#TODO: CHECK ON THIS, as a functio nof DIMENSION REDUCTION PARMETERS!
+	config_dict['nn']['layer_width'] = 2*50 #args.rb_rank 
+	config_dict['nn']['activation'] ='gelu'
+	# config_dict['nn']['layer_rank'] = 50 #nn_width = 2*args.rb_rank?
+	# config_dict['hidden_layer_dimensions'] = (config_dict['depth']-1)*[config_dict['truncation_dimension']]+[config_dict['fixed_output_rank']]
 
-nn_keys = ('architecture',) #'depth','layer_width', 'activation'
-data_keys =  ('data_dir','train_data_size','test_data_size',) #'data_filenames'
-optimization_keys =  ('step_size','batch_size','optax_optimizer','n_epochs')
-nn_serialization_keys =  ('network_name',)
-
-#Organize parameters into subdict heirarchy:
-config_dict = {}
-config_dict['nn'] = subdict(parent_dict=cli_args, 				
-									keys=nn_keys)
-config_dict['data'] = subdict(parent_dict=cli_args, 				
-							  keys=data_keys)
-config_dict['training'] = subdict(parent_dict=cli_args, 				
-							  	  keys=optimization_keys)
-config_dict['network_serialization'] = \
-	subdict(parent_dict=cli_args, 				
-			keys=nn_serialization_keys)
-
-# problem_config_dict = {} #is this necessary
-# config_dict['forward_problem'] = problem_config_dict
-
-# Neural Network Architecture parameters
-config_dict['nn']['depth'] = 6 
-#TODO: CHECK ON THIS, as a functio nof DIMENSION REDUCTION PARMETERS!
-config_dict['nn']['layer_width'] = 2*50 #args.rb_rank 
-config_dict['nn']['activation'] ='gelu'
-# config_dict['nn']['layer_rank'] = 50 #nn_width = 2*args.rb_rank?
-# config_dict['hidden_layer_dimensions'] = (config_dict['depth']-1)*[config_dict['truncation_dimension']]+[config_dict['fixed_output_rank']]
-
-#Encoder/Decoder parameters
-config_dict['encoder_decoder'] = {}
-config_dict['encoder_decoder']['encode'] = True
-config_dict['encoder_decoder']['decode'] = False
-config_dict['encoder_decoder']['encoder'] = cli_args['encoder_basis']
-config_dict['encoder_decoder']['decoder'] = 'pod' #ignored for now, decode is False
-encoder_decoder_dir = f"{cli_args['data_dir']}reduced_bases/" if cli_args['rb_dir']=='' else cli_args['rb_dir']
-encoder_basis = cli_args['encoder_basis'].lower()
-decoder_basis = cli_args['decoder_basis'].lower()
-if encoder_basis == 'kle':
-	encoder_cobasis_filename = 'KLE_cobasis.npy'
-	encoder_basis_filename = 'KLE_basis.npy'
-elif encoder_basis == 'as':
-	encoder_cobasis_filename = 'AS_encoder_cobasis.npy'
-	encoder_basis_filename = 'AS_encoder_basis.npy'
-else:
-	raise
-if 'full_state' in cli_args['data_dir']:
-	if decoder_basis == 'pod':
-		decoder_filename = 'POD_projector.npy'
+	#Encoder/Decoder parameters
+	config_dict['encoder_decoder'] = {}
+	config_dict['encoder_decoder']['encode'] = True
+	config_dict['encoder_decoder']['decode'] = False
+	config_dict['encoder_decoder']['encoder'] = cli_args['encoder_basis']
+	config_dict['encoder_decoder']['decoder'] = 'pod' #ignored for now, decode is False
+	encoder_decoder_dir = f"{cli_args['data_dir']}reduced_bases/" if cli_args['rb_dir']=='' else cli_args['rb_dir']
+	encoder_basis = cli_args['encoder_basis'].lower()
+	decoder_basis = cli_args['decoder_basis'].lower()
+	if encoder_basis == 'kle': #better approach?
+		encoder_cobasis_filename = 'KLE_cobasis.npy'
+		encoder_basis_filename = 'KLE_basis.npy' 
+	elif encoder_basis == 'as':
+		encoder_cobasis_filename = 'AS_encoder_cobasis.npy'
+		encoder_basis_filename = 'AS_encoder_basis.npy'
+	else:
+		raise
+	if 'full_state' in cli_args['data_dir']:
+		if decoder_basis == 'pod':
+			decoder_filename = 'POD_projector.npy'
+		else:
+			decoder_filename = None
 	else:
 		decoder_filename = None
-else:
-	decoder_filename = None
-config_dict['encoder_decoder']['save_location'] = \
-	cli_args['data_dir'] if cli_args['save_embedded_data'] else None
-config_dict['encoder_decoder']['encoder_decoder_dir'] = encoder_decoder_dir
-config_dict['encoder_decoder']['encoder_basis_filename'] = encoder_basis_filename
-config_dict['encoder_decoder']['encoder_cobasis_filename'] = encoder_cobasis_filename
-config_dict['encoder_decoder']['decoder_filename'] = decoder_filename
-# config_dict['encoder_decoder']['reduced_data_filenames'] = ('X_reduced.npy','Y_reduced.npy','J_reduced.npy') #these files may not exist
+	config_dict['encoder_decoder']['save_location'] = \
+		cli_args['data_dir'] if cli_args['save_embedded_data'] else None
+	config_dict['encoder_decoder']['encoder_decoder_dir'] = encoder_decoder_dir
+	config_dict['encoder_decoder']['encoder_basis_filename'] = encoder_basis_filename
+	config_dict['encoder_decoder']['encoder_cobasis_filename'] = encoder_cobasis_filename
+	config_dict['encoder_decoder']['decoder_filename'] = decoder_filename
+	# config_dict['encoder_decoder']['reduced_data_filenames'] = ('X_reduced.npy','Y_reduced.npy','J_reduced.npy') #these files may not exist
 
-# Data (Directory Location/Training) parameters
-if config_dict['encoder_decoder']['encode'] or config_dict['encoder_decoder']['decode']:
-	config_dict['data']['data_filenames'] = \
-		('m_data.npy','q_data.npy','J_data.npy')
-else:
-	config_dict['data']['data_filenames'] = \
-		('m_data.npy','q_data.npy','J_data.npy')
+	# Data (Directory Location/Training) parameters
+	if config_dict['encoder_decoder']['encode'] or config_dict['encoder_decoder']['decode']:
+		config_dict['data']['data_filenames'] = \
+			('m_data.npy','q_data.npy','J_data.npy')
+	else:
+		config_dict['data']['data_filenames'] = \
+			('m_data.npy','q_data.npy','J_data.npy')
 
-# Optimization parameters
-# config_dict['training']['shuffle_every_epoch'] = True
-loss_weights = [cli_args['l2_weight'],cli_args['h1_weight']]
-for loss_weight in loss_weights:
-	assert loss_weight >= 0
-config_dict['training']['loss_weights'] = loss_weights
+	# Optimization parameters
+	# config_dict['training']['shuffle_every_epoch'] = True
+	loss_weights = [cli_args['l2_weight'],cli_args['h1_weight']]
+	for loss_weight in loss_weights:
+		assert loss_weight >= 0
+	config_dict['training']['loss_weights'] = loss_weights
 
-# config_dict['truncation_dimension'] = args.truncation_dimension
+	# config_dict['truncation_dimension'] = args.truncation_dimension
 
-# Deserializing / Serializing Neural Network settings
-config_dict['network_serialization']['save_weights'] = True
-config_dict['network_serialization']['weights_dir'] = 'trained_weights/'
-# config_dict['network_serialization']['initial_guess_path'] = 
+	# Deserializing / Serializing Neural Network settings
+	config_dict['network_serialization']['save_weights'] = True #make a CLI
+	config_dict['network_serialization']['weights_dir'] = 'trained_weights/' #make a CLI
+	# config_dict['network_serialization']['initial_guess_path'] = 
 
-random_seed = cli_args['run_seed']
-if cli_args['l2_weight'] != 1.0:
-	config_dict['network_serialization']['network_name'] += f"l2_weight_{cli_args['l2_weight']}_seed_{random_seed}"
+	random_seed = cli_args['run_seed']
+	if cli_args['l2_weight'] != 1.0:
+		config_dict['network_serialization']['network_name'] += f"l2_weight_{cli_args['l2_weight']}_seed_{random_seed}"
 
-train_dino_in_embedding_space(model_key = jr.PRNGKey(random_seed), 
-							  embedded_training_config_dict=config_dict)
+	train_dino_in_embedding_space(model_key = jr.PRNGKey(random_seed), 
+								embedded_training_config_dict=config_dict)
+	return 0
 
 #2 digits in the F and 1 digit in the Jacobian
+
+
+# TODO: put this into train.py
+if __name__ == '__main__':
+    sys.exit(main())
+
